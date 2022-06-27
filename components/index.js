7417b50cc1beac0923bc220e192346fdac062550
r17x <ri7nz@evilfactory.id>
2022-06-28 01:38:49 +0700

    feat(navigation): add navigation for all routes

--- components/index.js
+++ components/index.js
@@ -1,7 +1,25 @@
-import { Box, ChakraProvider, Code, Container, Heading, ListItem, Text, UnorderedList, VStack } from "@chakra-ui/react";
+import { ArrowLeftShort as ArrowLeft } from "@chakra-icons/bootstrap";
+import {
+  Box,
+  Button,
+  ChakraProvider,
+  Code,
+  Container,
+  extendTheme,
+  Heading,
+  HStack,
+  IconButton,
+  Link,
+  ListItem,
+  Text,
+  UnorderedList,
+  VStack,
+} from "@chakra-ui/react";
 // eslint-disable-next-line
 import { CH } from "@code-hike/mdx/components";
 import { MDXProvider } from "@mdx-js/react";
+import NextLink from "next/link";
+import { useRouter } from "next/router";
 
 const withChildren =
   (Component, props) =>
@@ -42,11 +60,76 @@ const components = {
   CH,
 };
 
-export const AppProvider = ({ children }) => <ChakraProvider>{children}</ChakraProvider>;
+const theme = extendTheme({
+  components: {
+    Link: {
+      variants: {
+        primary: ({ colorScheme = "purple" }) => ({
+          color: colorScheme.concat(".500"),
+          _hover: {
+            color: colorScheme.concat(".400"),
+          },
+        }),
+      },
+      defaultProps: {
+        variant: "primary",
+      },
+    },
+  },
+});
+
+export const Navigation = () => {
+  const router = useRouter();
+
+  const left = (() => {
+    switch (router.pathname) {
+      case "/":
+        return <Button variant="outline">r17x</Button>;
+      default:
+        return (
+          <IconButton
+            color="currentcolor"
+            icon={<ArrowLeft boxSize="7" />}
+            onClick={router.back}
+            size="xl"
+            variant="link"
+          />
+        );
+    }
+  })();
+
+  return (
+    <HStack
+      alignItems="center"
+      backgroundColor="#121820"
+      bottom="0"
+      boxShadow="3xl"
+      justifyContent="space-between"
+      mb="16"
+      minW={["full", "full", "container.md", "container.md"]}
+      position="fixed"
+      px="3"
+      py="3"
+      rounded="md"
+      transform="translateY(-1.5em)"
+    >
+      <HStack alignItems="center">
+        <Link>{left}</Link>
+      </HStack>
+      <HStack alignItems="center">
+        <NextLink href="/posts" passHref>
+          <Link>Blog</Link>
+        </NextLink>
+      </HStack>
+    </HStack>
+  );
+};
+
+export const AppProvider = ({ children }) => <ChakraProvider theme={theme}>{children}</ChakraProvider>;
 
 export const Footer = ({ children }) => (
   <VStack borderTop="whitesmoke" mt="10vh" py="10vh">
-    <Text>{children}</Text>
+    <Text pb="5vh">{children}</Text>
   </VStack>
 );
 
@@ -55,6 +138,7 @@ export const Layout = ({ children }) => (
     <Container maxW="container.md" minH="96vh">
       {children}
     </Container>
+    <Navigation />
     <Footer>
       © {new Date().getFullYear()} - r17x with <Code>You</Code>
       {"."}
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,20 @@
 import { getLayout } from "../components";
 import { getAllPosts } from "../src";
 
-import { Code, Flex, Heading, Link, List, ListItem, Stack, Text, VStack } from "@chakra-ui/react";
+import { Github, Link as LinkIcon } from "@chakra-icons/bootstrap";
+import {
+  ButtonGroup,
+  Code,
+  Flex,
+  Heading,
+  IconButton,
+  Link,
+  List,
+  ListItem,
+  Stack,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
 import NextLink from "next/link";
 import { NextSeo } from "next-seo";
 
@@ -12,16 +25,43 @@ const Posts = ({ posts }) => (
   <List>
     {posts.map((post) => (
       <ListItem key={post.slug}>
-        <NextLink href={"/posts/".concat(post.slug)}>{post.title}</NextLink>
+        <NextLink href={"/posts/".concat(post.slug)}>
+          <Link colorScheme="whiteAlpha">{post.title}</Link>
+        </NextLink>
       </ListItem>
     ))}
   </List>
 );
 
-const Description = () => (
-  <VStack align="start" py="35vh" spacing="3">
+/** @typedef
+type SocialLink = {
+  label: string;
+  url: string;
+  icon: string
+};
+*/
+
+const getSocialLinkIcon = ({ icon }) => {
+  switch (icon) {
+    case "github":
+      return <Github />;
+    default:
+      return <LinkIcon />;
+  }
+};
+
+const _socialLinks = [
+  {
+    name: "Github: r17x",
+    url: "https://github.com/r17x",
+    icon: "github",
+  },
+];
+
+const Description = ({ socialLinks }) => (
+  <VStack align="start" py="30vh" spacing="3" transform="translateY(-70px)">
     <Heading as="h1" color="whiteAlpha.700">
-      {"ri7{x}"}
+      {"r17{x}"}
     </Heading>
     <Heading as="h2">
       a <Code>Software Engineer</Code>, Interest in topic (φ + Losophy), (λ + μετα-Programming), D.x (Developer
@@ -29,17 +69,11 @@ const Description = () => (
     </Heading>
     <Text>
       Thinkering open source projects at{" "}
-      <Link
-        color="purple.400"
-        fontWeight="bold"
-        href="https://evilfactorylabs.org"
-        target="_blank"
-        textDecoration="underline"
-      >
+      <Link fontWeight="bold" href="https://evilfactorylabs.org" target="_blank" textDecoration="underline">
         Evilfactorylabs.org
       </Link>
       {" & "}
-      <Link color="purple.400" fontWeight="bold" href="https://koding.ninja" target="_blank" textDecoration="underline">
+      <Link fontWeight="bold" href="https://koding.ninja" target="_blank" textDecoration="underline">
         KodingNinja
       </Link>
       {" . "}
@@ -47,6 +81,22 @@ const Description = () => (
     <Text>
       Inquiries via email at <Code>hi@rin.rocks</Code> or via Twitter at <Code> @__r17x </Code>
     </Text>
+    <ButtonGroup>
+      {socialLinks.map((social) => (
+        <NextLink key={social.url} href={social.url} passHref>
+          <Link rel="noopener noreferrer" target="_blank">
+            <IconButton
+              aria-label={social.label}
+              colorScheme="currentColor"
+              icon={getSocialLinkIcon(social)}
+              rounded="full"
+              size="md"
+              variant="outline"
+            />
+          </Link>
+        </NextLink>
+      ))}
+    </ButtonGroup>
   </VStack>
 );
 
@@ -54,7 +104,7 @@ const Home = ({ posts }) => (
   <>
     <NextSeo title="r17x - if you know, you know" />
     <Stack>
-      <Description />
+      <Description socialLinks={_socialLinks} />
       <Flex
         alignItems="center"
         borderBottomWidth="medium"
@@ -63,7 +113,9 @@ const Home = ({ posts }) => (
         pb="2"
       >
         <Heading as="h2">Recent Posts</Heading>
-        <Link href="/posts">see all posts</Link>
+        <NextLink href="/posts">
+          <Link color="white">see all posts</Link>
+        </NextLink>
       </Flex>
       <Posts posts={posts} />
     </Stack>
