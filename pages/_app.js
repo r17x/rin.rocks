dfab97fad237f8ac5bd813900eeb0aa0b98f2a28
r17x <ri7nz@evilfactory.id>
2022-11-07 22:39:12 +0700

    chore(deps): add @vercel/analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,18 @@
 import { AppProvider } from "../components";
 
+import { Analytics } from "@vercel/analytics/react";
+
 function MyApp({ Component, pageProps }) {
   const getLayout = Component.getLayout || ((x) => x);
 
   const children = getLayout(<Component {...pageProps} />);
 
-  return <AppProvider>{children}</AppProvider>;
+  return (
+    <AppProvider>
+      {children}
+      <Analytics />
+    </AppProvider>
+  );
 }
 
 export default MyApp;
