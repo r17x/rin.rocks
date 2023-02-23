f705e767a42f1feefbe2b8bbccf573d3f057c5f5
r17x <ri7nz@evilfactory.id>
2023-02-23 17:00:27 +0700

    fix(backend): fix 404 path when use navigation url

--- apps/backend/rin_rocks.ml
+++ apps/backend/rin_rocks.ml
@@ -1,12 +1,14 @@
-let loader _root path _request =
-  match Assets.read path with
-  | None -> Dream.empty `Not_Found
-  | Some asset -> Dream.respond asset
+let loader _root path request =
+  match Filename.extension path with
+  | "" -> Dream.redirect ~status:`Moved_Permanently request (String.concat "" [ "/"; path; ".html"; ])
+  | _ -> match Assets.read path with
+    | None -> Dream.empty `Not_Found
+    | Some asset -> Dream.respond asset
 
 let () =
   Dream.run
   @@ Dream.logger
   @@ Dream.router [
-    Dream.get "/" (fun req -> loader "" "index.html" req);
+    Dream.get "/" (loader "" "index.html");
     Dream.get "**" (Dream.static ~loader "");
   ]
